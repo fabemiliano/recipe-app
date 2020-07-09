1daa7a3756be2a309378d991ce24de7f3508fe94
fabemiliano <fab.emiliano@gmail.com>
2020-07-09 15:06:04 -0300

    Updates

---
+++ precache-manifest.eeb40ffab11751c98469cabf5dd83d73.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7c0f5d3aefe79d2461871dc1c8529d6",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "8782f3abb7a55394db7e",
+    "url": "/recipe-app/static/css/main.820768c7.chunk.css"
+  },
+  {
+    "revision": "b99b774112617c851c81",
+    "url": "/recipe-app/static/js/2.a1666863.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/recipe-app/static/js/2.a1666863.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8782f3abb7a55394db7e",
+    "url": "/recipe-app/static/js/main.9d9a5118.chunk.js"
+  },
+  {
+    "revision": "4b9365336cfc937175bb",
+    "url": "/recipe-app/static/js/runtime-main.b95f7359.js"
+  },
+  {
+    "revision": "ab0af8ef2aa20d3061f18bd6f2d65fa1",
+    "url": "/recipe-app/static/media/beef.ab0af8ef.svg"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/recipe-app/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "7779bd6d45c3bb5a289c36f187cca6d7",
+    "url": "/recipe-app/static/media/breakfast.7779bd6d.svg"
+  },
+  {
+    "revision": "f84f498822e60923334dc27876286c2b",
+    "url": "/recipe-app/static/media/chicken.f84f4988.svg"
+  },
+  {
+    "revision": "92e5d5418d8b812205e2468e226cd60f",
+    "url": "/recipe-app/static/media/dessert.92e5d541.svg"
+  },
+  {
+    "revision": "c61472f6a6b3c4f454a591d837f43582",
+    "url": "/recipe-app/static/media/drink.c61472f6.svg"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/recipe-app/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/recipe-app/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "37e525da7d9c067665e55ea81e4a6c37",
+    "url": "/recipe-app/static/media/filter.37e525da.svg"
+  },
+  {
+    "revision": "413f944a603d0c4029577de596197f8b",
+    "url": "/recipe-app/static/media/goat.413f944a.svg"
+  },
+  {
+    "revision": "e994a3e0196af60c708cb3f278a60b24",
+    "url": "/recipe-app/static/media/loginbackground.e994a3e0.jpg"
+  },
+  {
+    "revision": "6e3fa5c0d2909d34136e08ff07faf72a",
+    "url": "/recipe-app/static/media/meal.6e3fa5c0.svg"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/recipe-app/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/recipe-app/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/recipe-app/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/recipe-app/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/recipe-app/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
